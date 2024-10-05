4205cc6c27ea10829f2897839e99e12215ea885d
Bosona19 <161635012+Bosona19@users.noreply.github.com>
2024-10-05 19:33:51 +0300

    set up

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+ 
+function App() {
+  return (
+     <div></div>
+  );
+}
+
+export default App;
